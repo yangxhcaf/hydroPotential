18c6cf33ab8d61cf7a7f85328cd73fe34cf158ec
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-08-31 14:20:40 +0200

    major changes

--- src.R
+++ src.R
@@ -1,40 +1,50 @@
+### visualization scripts ###
+
+# load dependencies
+
 library(moinput)
 
+# manipulate magpie objects with aggregationss
+# "KOS" removed from eu11 as no counterpart in weighted predictions
+
+eu11 <- read.csv2("EU11.csv", stringsAsFactors = FALSE)
+eu11 <- eu11[-which(eu11[,1] == "KOS"),]
 generic <- readSource("Gernaat", "Tech.Full")
+generic <- generic[eu11[,1],,]
+genericAgg <- toolAggregate(generic, eu11)
+
 wgbu <- readSource("WGBU", convert = TRUE)
+wgbu <- wgbu[eu11[,1],,2]
+wgbuAgg <- toolAggregate(wgbu, eu11)
 
-genericAgg <- toolAggregate(generic, paste0(getConfig()$mappingfolder, "/regional/regionmapping_22_EU11.csv"))
-wgbuAgg <- toolAggregate(wgbu, paste0(getConfig()$mappingfolder, "/regional/regionmapping_22_EU11.csv"))
-wgbuAgg <- wgbuAgg[,,2]
+# convert to df
 
-### extra code ###
+wgbuDf <- as.data.frame(wgbuAgg)
+wgbuDf <- wgbuDf[order(wgbuDf[,2]),]
+ 
+genericDf <- as.data.frame(genericAgg)
+genericDf <- genericDf[order(genericDf[,2]),]
+ 
+# plot
 
-# make comparisons between both datasets, try map and convulation to show differences/similarities
-# need some world maps and boundaries to be able to conduct this
-# or possibly charts from east to west and show curves for different distributions
-# try different methods to show different aspects of data
+png("./vis/comparisonChart.png", width = 1200, height = 750)
+op <- par(cex = 1, mgp= c(2.8,1,0), oma=c(1,1,1,1))
+plot(1:12, wgbuDf$Value, "o", ylim = c(0,300), col = "red", lwd = 2, axes = F, xlab = "", ylab = "")
+box()
+par(new = TRUE)
+plot(1:12, genericDf$Value, "o", ylim = c(0,300), col = "blue", lwd = 2, axes = F, xlab = "EU11", ylab = "Technical Potential (TWh/a @ <0.5 $/Kwh)")
+box()
+axis(side = 1, at = c(1:12), labels = genericDf$Region)
+axis(side = 2, at = seq(0,500,50))
+legend(0.9, 290, legend=c("WGBU", "Gernaat et al. 2017"),
+       col=c("red", "blue"), lty=1:1, lwd = 2, cex=1.1)
+dev.off()
+
+### issues ###
 
 # can use generic for comparing technical and eocnomic potential differences, but not theoretical potentials
 # for theoretical potentials, one option could be to use the plos one shapefile data
 # another option would be to wait for the data provided by nature authors
-# check for existing plot functions available through local R packages, then experiment new methods
-
-# wgbuDf <- as.data.frame(wgbuAgg[,,2])
-# wgbuDf <- wgbuDf[order(wgbuDf[,2]),]
-# 
-# genericDf <- as.data.frame(generic[,,"Tech.Full"])
-# genericDf <- genericDf[order(genericDf[,2]),]
-# 
-# op <- par(mfrow=c(2,1), cex = 0.8, mgp= c(5,3,0))
-# plot(1:27, wgbuDf$Value, "o", ylim = c(0,2500), xlab = "WGBU", col = "red", lwd = 2, axes = F)
-# box()
-# axis(side = 1, at = c(1:27), labels = gsub(" ", "\n", genericDf$Region))
-# axis(side = 2, at = seq(0,2500,500))
-# plot(1:27, genericDf$Value, "o", ylim = c(0,2500), xlab = "Gernaat", col = "blue", lwd = 2, axes = F)
-# box()
-# axis(side = 1, at = c(1:27), labels = gsub(" ", "\n", genericDf$Region))
-# axis(side = 2, at = seq(0,2500,500))
- 
 # aggregate to remind regions
 # aggregate to europe eu11, important
 # bars on countries for easier difference visualization
